0442724530d36ce99549e6b6f8306cc2f4eeb9a1
samanthabd <donaldson.sgb@gmail.com>
2024-06-12 17:11:20 +0100

    add mutation observer

--- scripts/plugin.js
+++ scripts/plugin.js
@@ -3,40 +3,44 @@
   console.log(window)
   console.log('window.top')
   console.log(window.top)
-  
-  function observeMain() {
-    const targetNode = document.querySelector("body");
-    const config = {
-      attributes: false,
-      attributeOldValue: false,
-      childList: true,
-      subtree: true,
-    };
-    const callback = (mutationList, observer) => {
-      for (const mutation of mutationList) {
-        console.log(mutation)
-        let isAdditiveMutation = !!mutation.addedNodes.length
-        if (!isAdditiveMutation) {
-          continue
+
+  function init(w) {
+    function observeMain() {
+      const targetNode = w.document.querySelector("body");
+      const config = {
+        attributes: false,
+        attributeOldValue: false,
+        childList: true,
+        subtree: true,
+      };
+      const callback = (mutationList, observer) => {
+        for (const mutation of mutationList) {
+          console.log(mutation)
+          let isAdditiveMutation = !!mutation.addedNodes.length
+          if (!isAdditiveMutation) {
+            continue
+          }
+
+          // let addedNode = mutation.addedNodes[0];
+
+          // if (addedNode.classList.contains("memo")) {
+          //   let richText = addedNode.querySelector(".richText");
+          //   if (richText) markdownPre(richText);
+          // }
         }
-
-        // let addedNode = mutation.addedNodes[0];
-
-        // if (addedNode.classList.contains("memo")) {
-        //   let richText = addedNode.querySelector(".richText");
-        //   if (richText) markdownPre(richText);
-        // }
-      }
-        // if (mutation.target.className == "memos") {
-        //   observeMemosLoad()
-        //   observeMemoEdit()
-        //   // And now we don't need this observer anymore.
-        //   mainObserver.disconnect()
-        // }
-      }
-   // }
-    const mainObserver = new MutationObserver(callback);
-    mainObserver.observe(targetNode, config);
+          // if (mutation.target.className == "memos") {
+          //   observeMemosLoad()
+          //   observeMemoEdit()
+          //   // And now we don't need this observer anymore.
+          //   mainObserver.disconnect()
+          // }
+        }
+    // }
+      const mainObserver = new MutationObserver(callback);
+      mainObserver.observe(targetNode, config);
+    }
+    observeMain();
   }
-  observeMain();
+
+  init(window.top)
 })()
